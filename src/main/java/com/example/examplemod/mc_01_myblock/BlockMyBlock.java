4e9086c5da659e2ca222cf57be6bc3f2eadca989
earcra0106 <haji1003@outlook.jp>
2025-05-21 17:18:38 +0900

    feature: mock card application

--- src/main/java/com/example/examplemod/mc_01_myblock/BlockMyBlock.java
+++ src/main/java/com/example/examplemod/mc_01_myblock/BlockMyBlock.java
@@ -2,15 +2,13 @@ package com.example.examplemod.mc_01_myblock;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.HumanoidArm;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.animal.Pig;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 
@@ -28,6 +26,38 @@ public class BlockMyBlock extends Block{
         entityMob.setPos(pPos.getX(), pPos.getY(), pPos.getZ());
         if (!level.isClientSide) {
             ((ServerLevel) level).tryAddFreshEntityWithPassengers(entityMob);
+        }
+    }
+
+    @Override
+    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
+        super.stepOn(pLevel, pPos, pState, pEntity);
+
+        if (!pLevel.isClientSide) {
+            float explpsionRadius = 3.0f;
+
+            pLevel.explode(null, pPos.getX(), pPos.getY(), pPos.getZ(), explpsionRadius, Explosion.BlockInteraction.BREAK);
+        }
+    }
+
+    @Override
+    public void onPlace(BlockState pState, Level pLevel, BlockPos pPos, BlockState pOldState, boolean pIsMoving) {
+        super.onPlace(pState, pLevel, pPos, pOldState, pIsMoving);
+
+        if (pLevel.isClientSide) return;
+
+        for (int y = -1; y <= 1; y++) {
+            for (int x = -1; x <= 1; x++) {
+                for (int z = -1; z <= 1; z++) {
+                    BlockPos pos = pPos.offset(x, y, z);
+                    if (y == -1) {
+                        pLevel.setBlockAndUpdate(pos, Blocks.DIAMOND_BLOCK.defaultBlockState());
+                    } else {
+                        if (x == 0 && y == 0 && z == 0) continue;
+                        pLevel.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+                    }
+                }
+            }
         }
     }
 }
